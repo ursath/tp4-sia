36daa61bdac8c882df41a4ea0c830b3b52db925c
UrielArias <urielarias.a12@gmail.com>
2025-05-24 00:27:24 -0300

    feat: proccesing network output in main and changing format to allow a list of values for each variable based on config file

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from neural_networks.models.kohonen.kohonen_network import KohonenNetwork
-from neural_networks.similarity_functions import euclidean_distance
+from neural_networks.similarity_functions import euclidean_distance, exponential_similarity
+from normalization import Normalization
 import json
+import numpy as np
+from visualization import create_heatmap_for_kohonen_network
 
 if __name__ == '__main__':
     with open("config.json") as f:
         config = json.load(f)
 
-    similarity_functions = {"euclidean_distance": euclidean_distance}
+    similarity_functions = {"euclidean_distance": euclidean_distance, "exponential": exponential_similarity}
 
     df = pd.read_csv("input_data/europe.csv")  
     features = ['Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth', 'Unemployment']
@@ -17,13 +20,26 @@ if __name__ == '__main__':
     kohonen_config = config['kohonen']
 
     # calculate best k
-    k = int(kohonen_config['k'])
+    k_values = kohonen_config['k']
     selected_similarity_function = similarity_functions[kohonen_config['similarity_function']]
     initialize_random_weights = bool(kohonen_config['initialize_random_weights'])
 
-    R = float(kohonen_config['R'])
-    epochs = int(kohonen_config['epochs'])
-    learning_rate = float(kohonen_config['learning_rate'])
+    R_values = kohonen_config['R']
+    epoch_values = kohonen_config['epochs']
+    learning_rates = kohonen_config['learning_rate']
+    standarized_x = Normalization(x).standarize()
+    entries = standarized_x.astype(float).to_numpy()
 
-    kohonen_network = KohonenNetwork(x, len(features), k, selected_similarity_function, initialize_random_weights)
-    #kohonen_network.classify(R, epochs, learning_rate)
+    for k in k_values:
+        for R in R_values:
+            for epochs in epoch_values:
+                for learning_rate in learning_rates: 
+                    kohonen_network = KohonenNetwork(entries, len(features), k, selected_similarity_function, initialize_random_weights)
+                    entries_per_neuron, epoch = kohonen_network.classify(R, epochs, learning_rate)
+
+                    heatmap_data = np.empty((k, k), dtype=int)
+
+                    for (i,j), entry in np.ndenumerate(entries_per_neuron):
+                        heatmap_data[i, j] = len(entry)
+    
+                    create_heatmap_for_kohonen_network(heatmap_data,k, R, epochs)
