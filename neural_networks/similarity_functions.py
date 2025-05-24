230b7a7f24f3e113ead5ffd83af2ea5fe5d53bb6
UrielArias <urielarias.a12@gmail.com>
2025-05-24 00:20:47 -0300

    feat: adding exponential similarity function implementation

--- neural_networks/similarity_functions.py
+++ neural_networks/similarity_functions.py
@@ -4,3 +4,8 @@ def euclidean_distance(weights, x):
     basic_errors = weights - x
     squared_errors = basic_errors ** 2
     return np.sqrt(np.sum(squared_errors))
+
+def exponential_similarity(weights, x, alpha:float = 1.0):
+    basic_errors = weights - x
+    squared_errors = basic_errors ** 2
+    return np.exp(-alpha * squared_errors)
