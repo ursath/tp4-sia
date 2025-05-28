c2cd9ab0c073174759e9b6b290f7a0121b7199f0
UrielArias <urielarias.a12@gmail.com>
2025-05-28 00:44:38 -0300

    feat: adding ortogonality check for hopfield patterns

--- hopfield_main.py
+++ hopfield_main.py
@@ -25,11 +25,40 @@ if __name__ == "__main__":
     letter_pattern = [[int(x) for x in sublist] for sublist in letter_pattern[0]]
     flat_letter_pattern = [line for sublist in letter_pattern for line in sublist]
 
+    np_patterns = []    
+    for pattern in patterns:
+        np_patterns.append(np.array(pattern))
+
+    ortogonality_matrix = np.zeros((len(patterns), len(patterns)))
+    for i, first_pattern in enumerate(np_patterns):
+        for j, second_pattern in enumerate(np_patterns):
+            if not np.array_equal(first_pattern, second_pattern):
+                ortogonality_matrix[i][j] = np.dot(first_pattern, second_pattern)
+
+    print("ortogonality check:")
+    for i in range(len(patterns)):
+        print(ortogonality_matrix[i])
 
     hopfield_network = HopfieldNetwork(patterns)
     hopfield_network.initialize_weights()
+    # check weights 
+    weights = open("output/hopfield_network_weights.txt", "r").readlines()
+    weight_matrix = np.zeros((len(patterns[0]), len(patterns[0])))
+    for row, line in enumerate(weights):
+        weights_row = line.strip().split(" ")
+        for col, weight in enumerate(weights_row):
+            weight_matrix[row][col] = float(weight)
 
-    state_vector = hopfield_network.classify(flat_letter_pattern)
+    symetric = True
+    for row in range(len(patterns[0])):
+        for column in range(len(patterns[0])):
+            if weight_matrix[row, col] != weight_matrix[col, row]:
+                symetric = False
 
+    #print("The weight matrix is ok") if symetric else print("The weight matrix is not symetric")
+
+    epoch, state_vector = hopfield_network.classify(np.array(flat_letter_pattern))
+
+    print("obtained output:")
     for i in range(0, len(state_vector), 5):
         print(state_vector[i:i+5])
